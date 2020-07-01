51e05fd5124f56a584e580afe0aaa6533445722c
nazarleskiw <41113704+nazarleskiw@users.noreply.github.com>
2020-07-01 14:30:30 +0200

    Add files via upload

---
+++ 3pipe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "functions.h"
+
+// 3. "Pipe":
+// entry: pipe
+// Modification/exit x: bit set at the position set by x
+
+int main(int argc, char *argv[])
+{
+    programId(3);
+    unsigned int value;
+    unsigned int result;
+    char str[10];
+    getValue(&value);
+
+
+}
