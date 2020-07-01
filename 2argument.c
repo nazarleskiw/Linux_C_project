1e6beb17fb5b27f33b8c86339a7701bcccccf334
nazarleskiw <41113704+nazarleskiw@users.noreply.github.com>
2020-07-01 14:27:20 +0200

    Add files via upload

---
+++ 2argument.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "functions.h"
+
+// 2. "Argument":
+// entry: getopt
+// Modification/exit x: next prime number
+
+int main(int argc, char *argv[])
+{
+    programId(2);
+    unsigned int value;
+    unsigned int result;
+    char str[10];
+    value = parseGetopt(argc, argv);
+
+
+}
