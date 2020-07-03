024b3968d6fe9a6803c0f544edbcba2213c60ff1
nazarleskiw <41113704+nazarleskiw@users.noreply.github.com>
2020-07-03 22:48:41 +0200

    Update 2argument.c

--- 2argument.c
+++ 2argument.c
@@ -10,8 +10,15 @@ int main(int argc, char *argv[])
     programId(2);
     unsigned int value;
     unsigned int result;
-    char str[10];
+    char str[15];
     value = parseGetopt(argc, argv);
+    functionPrime(value, &result, str);
+
+    printf("An argument was sent to program number 3: %d\n", result);
+
+    int fd;
+    char *myfifo = "/tmp/myfifo";
+    mkfifo(myfifo, 0666);
 
 
 }
